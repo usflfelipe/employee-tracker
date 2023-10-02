de733b60afa99b6f32e41a2b72376aa30a75b78f
Felipe Ursini <usfl.felipe@gmail.com>
2023-10-02 01:13:46 -0400

    added more code

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department
+  (name)
+VALUES
+  ('Engineering'),
+  ('Sales'),
+  ('Finance'),
+  ('Legal');
+
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Software Engineer', 85000, 1),
+  ('Salesperson', 75000, 2),
+  ('Accountant', 125000, 3),
+  ('Lawyer', 200000, 4);
+
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Juan', 'Garcia', 1, 4),
+  ('Jonathan', 'Villcapoma', 2, 3),
+  ('Jesus', 'Meraz', 3, 1),
+  ('Estefany', 'Munoz', 4, 5);
